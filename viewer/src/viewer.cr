8a8412cdb6a2bcba5757554633799861232aed4d
Bluek404 <i@bluek404.net>
2015-06-23 18:25:19 +0800

    使用SQLite

--- viewer/src/viewer.cr
+++ viewer/src/viewer.cr
@@ -1,11 +1,21 @@
 require "json"
 require "./viewer/*"
 require "../../src/spider/comment"
+require "sqlite3"
 
 module Viewer
   def self.run(dir)
+    db = SQLite3::Database.new "data.db"
+    db.execute %{
+      CREATE TABLE IF NOT EXISTS comments (
+        content VARCHAR(2048),
+        oo      INT,
+        xx      INT)}
+    ins_stmt = db.prepare %{
+      INSERT INTO comments
+      VALUES (?, ?, ?)}
     comments = Array(Spider::Comment).new
-    Dir.foreach dir, do |f|
+    Dir.foreach(dir) do |f|
       next if f == "." || f == ".."
       next if File.directory? f
       next unless f =~ /.*.json/
@@ -14,6 +24,13 @@ module Viewer
       json = Array(Spider::Comment).from_json str
       comments.concat json
     end
+    comments.each do |comment|
+      p comment.content
+      result_set = ins_stmt.execute comment.content, comment.oo, comment.xx
+      result_set.next
+    end
+    ins_stmt.close
+    db.close
   end
 end
 
